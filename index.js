81283bca7b608d45a9aac67ece998640f85b5171
makotot <makoto.ttn@gmail.com>
2015-09-11 13:30:00 +0900

    rm unused dependencies

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = function (params, cb) {
-  var ext = 'hbs',
-    assembleOptions = params.assemble.options,
+  var assembleOptions = params.assemble.options,
     pageInfoList = assembleOptions.pages,
     pages = pageInfoList.map(function (page) {
       return page.src;
